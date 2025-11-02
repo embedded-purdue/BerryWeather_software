8663528512aa81f518bf41b6f1f441d9dada816c
Carman333 <68566279+Carman333@users.noreply.github.com>
2025-11-02 13:45:31 -0500

    commit

--- esp32_skeleton/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
+++ esp32_skeleton/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -1,27 +1,27 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-# file LICENSE.rst or https://cmake.org/licensing for details.
+# file Copyright.txt or https://cmake.org/licensing for details.
 
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+cmake_minimum_required(VERSION 3.5)
 
 # If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
 # existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
 # would cause a fatal error, even though it would be a no-op.
-if(NOT EXISTS "/Users/connorpowell/esp/v5.5.1/esp-idf/components/bootloader/subproject")
-  file(MAKE_DIRECTORY "/Users/connorpowell/esp/v5.5.1/esp-idf/components/bootloader/subproject")
+if(NOT EXISTS "C:/Users/carma/esp/v5.5.1/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "C:/Users/carma/esp/v5.5.1/esp-idf/components/bootloader/subproject")
 endif()
 file(MAKE_DIRECTORY
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader"
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix"
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/tmp"
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp"
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/src"
-  "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/tmp"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/src"
+  "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+    file(MAKE_DIRECTORY "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/Users/connorpowell/Documents/Purdue/ES@P/BerryWeather/BerryWeather/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "C:/Users/carma/OneDrive/Documents/GitHub/BerryWeather_software/esp32_skeleton/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
 endif()
